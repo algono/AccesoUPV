b1f28d909d8c76b0204af62bab2f76e38c4aba26
Alejandro Gómez <algono@inf.upv.es>
2019-01-18 13:09:18 +0100

    Estructura inicial

--- AccesoUPV/AccesoUPV/Program.cs
+++ AccesoUPV/AccesoUPV/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace AccesoUPV
+namespace AccesoUPV.GUI
 {
     static class Program
     {
---
+++ AccesoUPV/AccesoUPVLib/AccesoUPV.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoUPV.Lib
+{
+    public class AccesoUPV
+    {
+    }
+}
