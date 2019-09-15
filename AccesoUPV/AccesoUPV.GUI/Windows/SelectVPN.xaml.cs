52d9d82be75beb829097579e42e938546cc2c741
Alejandro Gómez <algono@inf.upv.es>
2019-09-15 22:50:45 +0200

    VPN creation checks if name already exists

--- AccesoUPV/AccesoUPV.GUI/Windows/SelectVPN.xaml.cs
+++ AccesoUPV/AccesoUPV.GUI/Windows/SelectVPN.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using AccesoUPV.Library.Connectors.VPN;
 using Microsoft.VisualBasic;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,23 +16,39 @@ namespace AccesoUPV.GUI.Windows
         public VPN SelectedVPN { get; }
         public string SelectedName => SelectedVPN.Name;
 
+        private List<string> _nameList;
+        private readonly List<string> _actualNameList = new List<string>();
+        public List<string> NameList
+        {
+            get => _nameList;
+            private set
+            {
+                _nameList = value;
+
+                _actualNameList.Clear();
+                if (AllowCreation) _actualNameList.Add(CreateNewVPNItemText);
+                _actualNameList.AddRange(_nameList);
+            }
+        }
+
         public string Server => SelectedVPN.Config.Server;
 
         public bool Canceled { get; private set; }
 
         private bool _allowCreation;
+
         public bool AllowCreation
         {
             get => _allowCreation;
             set
             {
-                if (value)
+                if (!_allowCreation && value)
                 {
-                    VPNList.Items.Insert(0, CreateNewVPNItemText);
+                    _actualNameList.Insert(0, CreateNewVPNItemText);
                 }
-                else
+                else if (AllowCreation && !value)
                 {
-                    VPNList.Items.Remove(CreateNewVPNItemText);
+                    _actualNameList.Remove(CreateNewVPNItemText);
                 }
 
                 _allowCreation = value;
@@ -39,12 +56,13 @@ namespace AccesoUPV.GUI.Windows
         }
 
         private const string CreateNewVPNItemText = @"<Crear nueva>";
+        private const string VPNAlreadyExistsMessage = "Ya existe una conexión VPN con el nombre indicado.";
 
         public SelectVPN(VPNConfig config, bool allowCreation = true) : this(new VPN(config), allowCreation)
         {
 
         }
-        
+
         public SelectVPN(string server, bool allowCreation = false) : this(new VPN(server), allowCreation)
         {
 
@@ -54,6 +72,7 @@ namespace AccesoUPV.GUI.Windows
         {
             InitializeComponent();
             SelectedVPN = vpn;
+            VPNList.ItemsSource = _actualNameList;
             _allowCreation = allowCreation;
 
             this.Loaded += async (sender, e) =>
@@ -65,12 +84,9 @@ namespace AccesoUPV.GUI.Windows
 
         private async Task LoadNameList()
         {
-            IList<string> list = Server == null
+            NameList = Server == null
                 ? await VPN.GetNameListAsync()
                 : await VPNConfig.FindNamesAsync(Server);
-
-            if (AllowCreation) list.Insert(0, CreateNewVPNItemText);
-            VPNList.ItemsSource = list;
         }
 
         private void ShowList()
@@ -104,12 +120,24 @@ namespace AccesoUPV.GUI.Windows
             {
                 // The user canceled the operation
             }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, null, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private async Task CreateNewVPN()
         {
             string newName = Interaction.InputBox("Introduzca el nombre de la nueva conexión VPN:", "Nueva conexión VPN");
-            if (string.IsNullOrEmpty(newName)) throw new OperationCanceledException();
+
+            if (string.IsNullOrEmpty(newName))
+            {
+                throw new OperationCanceledException();
+            }
+            else if (NameList?.Contains(newName, new VPNNameEqualityComparer()) ?? false)
+            {
+                throw new ArgumentOutOfRangeException(null, VPNAlreadyExistsMessage);
+            }
             else
             {
                 SelectedVPN.Name = newName;
---
+++ AccesoUPV/AccesoUPV.Library/Connectors/VPN/VPNNameEqualityComparer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AccesoUPV.Library.Connectors.VPN
+{
+    public class VPNNameEqualityComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string x, string y) => string.Equals(x, y, StringComparison.OrdinalIgnoreCase);
+
+        public int GetHashCode(string obj) => obj.GetHashCode();
+    }
+}
